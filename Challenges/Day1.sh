dcf3bef91a630053d60c1062fedcf02d4e1bd7f0
Saurav05 <sauravkhatri05@gmail.com>
2023-07-31 17:10:00 +0000

    Day 1 Completed

---
+++ Challenges/Day1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Task1
+#BashBlaze Day1 Challange Let's learn the Bash With Daily challange 
+
+#Task 2 Echo to display messages to terminal 
+echo "Day1 BashBlaze"
+
+#Task 3 Declaring Varibles 
+name='saurav'
+
+#Task 4 Using the Declared  Variable 
+echo "My name is $name"
+
+#Task 5 Using Built in Variable 
+echo "Currently I'm using $BASH and it's version is $BASH_VERSINFO with process id $BASHPID"
+
+#Task 6 WildCards this is used to list all the  scripting files present on current working directory
+ls *.sh
+
