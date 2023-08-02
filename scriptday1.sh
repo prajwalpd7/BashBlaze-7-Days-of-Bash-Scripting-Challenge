b9ceaa35172a1292e2a655ab92778077508aceab
Neha Garg <nehagarg.94@gmail.com>
2023-08-02 14:56:23 -0600

    Create scriptday1.sh

---
+++ scriptday1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script is a part of bash scripting challenge
+echo "hey, there!"
+name="Neha Garg"
+echo $name
+echo $HOSTNAME
+echo $USERNAME
+echo $BASH_VERSION
+echo "files with .txt extension"
+ls *.txt
