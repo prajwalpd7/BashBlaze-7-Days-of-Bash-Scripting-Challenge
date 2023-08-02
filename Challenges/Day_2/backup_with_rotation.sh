c7275abfbed85112b6ccc7ef151c0b225ec2818d
Kunal Gohrani <gohranikunal25@outlook.com>
2023-08-02 12:50:19 +0000

    Updating directory structure of Day 2

--- Challenges/Day_2/backup_with_rotation.sh
+++ Challenges/Day_2/backup_with_rotation.sh
@@ -1,41 +1,60 @@
 #!/bin/bash
 
-# Function to display usage information and available options
-function display_usage {
-    echo "Usage: $0 /path/to/source_directory"
+# In this function, we are checking if the number of backup folders is greater than 3, if so,
+# then we only keep the latest 3 backups and delete others
+rotate_backups() {
+ backup=$1
+ NUMBER_OF_BACKUPS_ALLOWED=3
+ NUMBER_OF_FOLDERS_PRESENT=$( ls $backup | sort -V | wc -l  )
+ NUMBER_OF_FOLDERS_TO_BE_REMOVED=$(( $NUMBER_OF_FOLDERS_PRESENT - $NUMBER_OF_BACKUPS_ALLOWED ))
+
+ if [[ $NUMBER_OF_FOLDERS_TO_BE_REMOVED -gt 0 ]]; then
+	 FOLDERS_TO_BE_DELETED=$( ls $backup | sort -V | head -n $NUMBER_OF_FOLDERS_TO_BE_REMOVED )
+	 for folder in $FOLDERS_TO_BE_DELETED;
+	 do
+		 rm -r $backup/$folder
+	 done
+ fi
 }
 
-# Check if a valid directory path is provided as a command-line argument
-if [ $# -eq 0 ] || [ ! -d "$1" ]; then
-    echo "Error: Please provide a valid directory path as a command-line argument."
-    display_usage
+# Starting of script
+if [[ $# -ne 2 ]]; then
+	echo "Correct Usage: ./backup_with_rotation.sh <src_directory> <target_directory>"
+	exit 1
+fi
+
+
+# Assigning command line arguments to variables
+source_directory="$1"
+target_directory="$2"
+timestamp=$(date +%Y%m%d%H%M%S)
+backup_directory="$target_directory/backup_$timestamp"
+
+# Validate input: check if source directory exists
+if [ ! -d "$source_directory" ]; then
+    echo "Error: Source directory '$source_directory' does not exist."
     exit 1
 fi
 
-# Directory path of the source directory to be backed up
-source_dir="$1"
-
-# Function to create a timestamped backup folder
-function create_backup {
-    local timestamp=$(date '+%Y-%m-%d_%H-%M-%S')  # Get the current timestamp
-    local backup_dir="${source_dir}/backup_${timestamp}"
-
-    # Create the backup folder with the timestamped name
-    mkdir "$backup_dir"
-    echo "Backup created successfully: $backup_dir"
-}
-
-# Function to perform the rotation and keep only the last 3 backups
-function perform_rotation {
-    local backups=($(ls -t "${source_dir}/backup_"* 2>/dev/null))  # List existing backups sorted by timestamp
-
-    # Check if there are more than 3 backups
-    if [ "${#backups[@]}" -gt 3 ]; then
-        local backups_to_remove="${backups[@]:3}"  # Get backups beyond the last 3
-        rm -rf "${backups_to_remove[@]}"  # Remove the oldest backups
+# Create target directory if not present
+if [ ! -d "$backup_directory" ]; then
+    mkdir -p "$target_directory"
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to create target directory '$target_directory'."
+        exit 1
     fi
-}
+fi
 
-# Main script logic
-create_backup
-perform_rotation
+# Copying files from src to target directory
+cp -r $source_directory "$backup_directory/"
+
+# if copy was successful, then we will check for old backup folders that need to be deleted
+if [ $? -eq 0 ]; then
+    echo "Backup created successfully at: $backup_directory"
+    rotate_backups $target_directory
+else
+    echo "Error: Failed to create backup."
+    exit 1
+fi
+
+exit 0
