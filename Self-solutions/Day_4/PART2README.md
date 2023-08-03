6311495d25de43055a6829d2b79b20da92b8aa5c
salvathshaik <salvathshaik786143@gmail.com>
2023-08-03 17:17:36 +0000

    Added day-4 part2 solution

---
+++ Self-solutions/Day_4/PART2README.md
@@ -0,0 +1,161 @@
+please run day4_part2_challenge_solution.sh file.
+
+sample output of this script as follows
+
+root@ip-172-31-91-38:~/BashBlaze-7-Days-of-Bash-Scripting-Challenge/Self-solutions/Day_4# ./day4_part2_challenge_solution.sh
+
+Main Menu
+
+------ Monitoring Metrics Script -------
+
+Choose below options
+    1. View System Metrics
+    2. Monitor a Specific Service
+    3. Exit
+
+Enter your option: 1
+CPU Usage: 6.20%   Mem Usage: 26.00%  Disk Usage: 39%%
+
+Main Menu
+
+------ Monitoring Metrics Script -------
+
+Choose below options
+    1. View System Metrics
+    2. Monitor a Specific Service
+    3. Exit
+
+Enter your option: 1
+
+Showing metrics again in 5 seconds... please wait!
+
+CPU Usage: 0.00%   Mem Usage: 26.00%  Disk Usage: 39%%
+
+Main Menu
+
+------ Monitoring Metrics Script -------
+
+Choose below options
+    1. View System Metrics
+    2. Monitor a Specific Service
+    3. Exit
+
+Enter your option: 2
+Enter the name of the service to monitor: nginx
+
+Mentioned process 'nginx' is running in the system !!!
+● nginx.service - A high performance web server and a reverse proxy server
+     Loaded: loaded (/lib/systemd/system/nginx.service; enabled; vendor preset: enabled)
+     Active: active (running) since Thu 2023-08-03 13:44:00 UTC; 3h 19min ago
+       Docs: man:nginx(8)
+    Process: 47056 ExecStartPre=/usr/sbin/nginx -t -q -g daemon on; master_process on; (code=exited, status=0/SUCCESS)
+    Process: 47057 ExecStart=/usr/sbin/nginx -g daemon on; master_process on; (code=exited, status=0/SUCCESS)
+   Main PID: 47058 (nginx)
+      Tasks: 2 (limit: 1141)
+     Memory: 2.7M
+        CPU: 171ms
+     CGroup: /system.slice/nginx.service
+             ├─47058 "nginx: master process /usr/sbin/nginx -g daemon on; master_process on;"
+             └─47060 "nginx: worker process" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" ""
+
+Aug 03 13:44:00 ip-172-31-91-38 systemd[1]: Starting A high performance web server and a reverse proxy server...
+Aug 03 13:44:00 ip-172-31-91-38 systemd[1]: Started A high performance web server and a reverse proxy server.
+
+Main Menu
+
+------ Monitoring Metrics Script -------
+
+Choose below options
+    1. View System Metrics
+    2. Monitor a Specific Service
+    3. Exit
+
+Enter your option: 4
+Invalid option '4'. please try again wtih the availiable options.
+
+Main Menu
+
+------ Monitoring Metrics Script -------
+
+Choose below options
+    1. View System Metrics
+    2. Monitor a Specific Service
+    3. Exit
+
+Enter your option: 3
+
+Exiting ..
+
+
+Stopping the nginx service manually to check this scenario
+
+root@ip-172-31-91-38:~/BashBlaze-7-Days-of-Bash-Scripting-Challenge/Self-solutions/Day_4# systemctl stop nginx
+
+root@ip-172-31-91-38:~/BashBlaze-7-Days-of-Bash-Scripting-Challenge/Self-solutions/Day_4# ./day4_part2_challenge_solution.sh
+
+Main Menu
+
+------ Monitoring Metrics Script -------
+
+Choose below options
+    1. View System Metrics
+    2. Monitor a Specific Service
+    3. Exit
+
+Enter your option: 2
+Enter the name of the service to monitor: nginx
+
+Mentioned service 'nginx' is not running in the system...
+
+Do you want to start nginx? (Y/N): Y
+'nginx' service started successfully!!
+● nginx.service - A high performance web server and a reverse proxy server
+     Loaded: loaded (/lib/systemd/system/nginx.service; enabled; vendor preset: enabled)
+     Active: active (running) since Thu 2023-08-03 17:07:11 UTC; 17ms ago
+       Docs: man:nginx(8)
+    Process: 48414 ExecStartPre=/usr/sbin/nginx -t -q -g daemon on; master_process on; (code=exited, status=0/SUCCESS)
+    Process: 48415 ExecStart=/usr/sbin/nginx -g daemon on; master_process on; (code=exited, status=0/SUCCESS)
+   Main PID: 48416 (nginx)
+      Tasks: 2 (limit: 1141)
+     Memory: 2.5M
+        CPU: 22ms
+     CGroup: /system.slice/nginx.service
+             ├─48416 "nginx: master process /usr/sbin/nginx -g daemon on; master_process on;"
+             └─48418 "nginx: worker process" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" ""
+
+Aug 03 17:07:11 ip-172-31-91-38 systemd[1]: Starting A high performance web server and a reverse proxy server...
+Aug 03 17:07:11 ip-172-31-91-38 systemd[1]: Started A high performance web server and a reverse proxy server.
+
+
+Main Menu
+
+------ Monitoring Metrics Script -------
+
+Choose below options
+    1. View System Metrics
+    2. Monitor a Specific Service
+    3. Exit
+
+Enter your option: 2
+Enter the name of the service to monitor: dummy-nginx-service
+
+Mentioned service 'dummy-nginx-service' is not running in the system...
+
+Do you want to start dummy-nginx-service? (Y/N): Y
+Failed to start dummy-nginx-service.service: Unit dummy-nginx-service.service not found.
+Failed to start the service dummy-nginx-service. Please check the service name and try again!!!
+
+Main Menu
+
+------ Monitoring Metrics Script -------
+
+Choose below options
+    1. View System Metrics
+    2. Monitor a Specific Service
+    3. Exit
+
+Enter your option:3
+
+Exiting ..
+
+
--- Self-solutions/Day_4/day4_part2_challenge_solution.sh
+++ Self-solutions/Day_4/day4_part2_challenge_solution.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+#####################################################################
+# Script Name: day4_part2_challange_solution.sh
+# Author: Salwad Basha Shaik
+# Date: August 3, 2023
+# Description: This script contains solution for Day 4 part-2 Monitoring Metrics Script with Sleep Mechanism as part of BashBlaze: 7 Days of Bash Scripting Challenge.
+# Usage: ./day4_part2__challange_solution.sh
+# Email: salvathshaik786143@gmail.com
+# LinkedIn: https://www.linkedin.com/in/salwad-basha-shaik/
+# Github: https://github.com/salvathshaik/
+#####################################################################
 
+#set -x
 ####################################################  Day-4 part2 Monitoring Metrics Script with Sleep Mechanism    #############################
 #######################  Challenge Description   ####################
 #This project aims to create a Bash script that monitors system metrics like CPU usage, memory usage, and disk space usage. The script will provide a user-friendly interface, allow continuous monitoring with a specified sleep interval, and extend its capabilities to monitor specific services like Nginx.
@@ -30,7 +41,118 @@
 
 #Remember, the main goal of this challenge is to utilize various monitoring commands, implement a user-friendly interface, and create a script that is easy to understand and use.
 
+check_if_process_running() {
 
+	process_name=$1
+	
+	if pgrep "$process_name" >/dev/null; then  #checking if provided process is running or not
+						   #The >/dev/null part in the shell script is used for redirecting the standard output of a command to the null dev							          -ice (/dev/null),which is a special device that discards any data written to it.
+						   #If the process is running, pgrep will return a success status (exit code 0), and the output will be discarded. 
+						   # If the process is not running, pgrep will return a non-zero status, and the output will be discarded as well.
+						   #The if statement checks the exit status, and based on that, it print the appropriate message without showing any 								output from the pgrep command itself.
+		return 0
+	else
+		return 1
+	fi
+}
+
+
+while true; do
+	echo
+	echo "Main Menu"
+	echo
+	echo "------ Monitoring Metrics Script -------"
+	echo
+	echo "Choose below options"
+	echo "    1. View System Metrics "
+	echo "    2. Monitor a Specific Service "
+	echo "    3. Exit"
+	echo
+
+	read -p "Enter your option: " userOption
+    	
+	case $userOption  in
+		1) 
+			
+			if [ "$reEnterTheMetrix" == 1  ]; then
+				echo
+				echo "Showing metrics again in 5 seconds... please wait! "
+				echo
+				sleep 5   #implementing the sleep mechanism if user wants to see metric details from second timewe will show the metrics detaisl with hiafter 5 seonds.
+				reEnterTheMetrix=0
+		
+			fi
+			cpu_metric_usage=$( top -bn1 | grep 'Cpu(s)' | awk '{print $2 + $4}' )  #the awk command is used to extract specific columns from the output of the top command.  and '{print $2 + $4}': will sum the second  %CPU column from the output of the top command and $4: Represents the fourth column of the input (in this case, the %us column, which represents the percentage of CPU used by user processes).
+
+			cpu_metric_usage_percentage=$( printf "%.2f" "$cpu_metric_usage" ) #displaying only 2 decimal values.
+			
+			memory_metric_total=$( free | grep 'Mem:' | awk '{print $2}' )  #this free command will give us the memory usage details. and here we are taking 2nd and 3rd argument of free command output and calculating the percentage.
+			memory_metric_total_used=$( free | grep 'Mem:' | awk '{print $3}' )
+			memory_metric_usage_percentage=$(echo "scale=2; $memory_metric_total_used / $memory_metric_total * 100" | bc) 
+							#bc: basic calculator operation and to  store only two decimal value as a output we are using scal=2.
+			
+			disk_usage_metric=$( df -h / | awk 'NR == 2 {print $5}' )   #here NR===2 means taking the second line from df -h / disk space outpur and extracting 5th argument as a disk usage metric.
+
+			echo "CPU Usage: $cpu_metric_usage_percentage%   Mem Usage: $memory_metric_usage_percentage%  Disk Usage: $disk_usage_metric%"
+			
+			reEnterTheMetrix=1
+
+			;;
+        	
+		
+		2)
+
+			read -p "Enter the name of the service to monitor: "  serviceNameByUser #taking service nameinput to monitor.
+			check_if_process_running "$serviceNameByUser" #passing the service name to the function.
+			
+			if [ $? == 0  ]; then   #comparing the output of above function usign $? with 0 means service is running.
+				echo
+				echo "Mentioned process '$serviceNameByUser' is running in the system !!!"
+				sudo systemctl status "$serviceNameByUser"   #displaying the status of process.
+
+			else
+				echo
+				echo "Mentioned service '$serviceNameByUser' is not running in the system..."
+				echo
+				
+				read -p "Do you want to start $serviceNameByUser? (Y/N): " userOptionToStart
+
+				if [ "$userOptionToStart" == "Y"  ]; then
+					sudo systemctl start "$serviceNameByUser" #using systemctl command to start the service.
+
+					check_if_process_running $serviceNameByUser
+					if [ $? == 0  ]; then
+						echo "'$serviceNameByUser' service started successfully!! "
+				        	sudo systemctl status $serviceNameByUser	
+						echo
+					else
+						echo "Failed to start the service $serviceNameByUser. Please check the service name and try again!!!"
+					fi
+				else	
+					echo "We did not try to start the service, Exiting..."
+				fi
+			fi
+
+			;;
+
+
+
+		3)
+			echo
+			echo "Exiting .."
+			echo
+			break
+
+			;;
+
+
+		*)
+            		echo "Invalid option '$userOption'. please try again wtih the availiable options."
+
+            		;;
+    	esac
+  	
+done
 
 
 
