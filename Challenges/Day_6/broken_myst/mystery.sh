9db7e1a49377bb6b6a917add7815ff6938ab587b
yash91989201 <yashraj6370@gmail.com>
2023-08-06 07:25:08 +0530

    added solution for mystery bash script for day 6

--- Challenges/Day_6/broken_myst/mystery.sh
+++ Challenges/Day_6/broken_myst/mystery.sh
@@ -1,43 +1,25 @@
 #!/bin/bash
-
-# Welcome to the Mysterious Script Challenge!
-# Your task is to unravel the mystery behind this script and understand what it does.
-# Once you've deciphered its objective, your mission is to improve the script by adding comments and explanations for clarity.
-
-# DISCLAIMER: This script is purely fictional and does not perform any harmful actions.
-# It's designed to challenge your scripting skills and creativity.
-
 # The Mysterious Function
 mysterious_function() {
-    local input_file="$1"
-    local output_file="$2"
-    
-    # 
+    local input_file="$1"  # Store the input file path
+    local output_file="$2" # Store the output file path
+    # Step 1: Apply a ROT13-like cipher to input_file and save the result in output_file
     tr 'A-Za-z' 'N-ZA-Mn-za-m' < "$input_file" > "$output_file"
-
-    # 
+    # Step 2: Reverse the text from output_file and save it in reversed_temp.txt
     rev "$output_file" > "reversed_temp.txt"
-
-    # 
+    # Step 3: Generate a random number between 1 and 10
     random_number=$(( ( RANDOM % 10 ) + 1 ))
-
-    # Mystery loop: 
+    # Repeat the following block of operations random_number times
     for (( i=0; i<$random_number; i++ )); do
-        # 
+        # Step 4: Reverse the text from reversed_temp.txt and save it in temp_rev.txt
         rev "reversed_temp.txt" > "temp_rev.txt"
-
-        # 
+        # Step 5: Apply the same ROT13-like cipher to temp_rev.txt and save it in temp_enc.txt
         tr 'A-Za-z' 'N-ZA-Mn-za-m' < "temp_rev.txt" > "temp_enc.txt"
-
-        # 
+        # Step 6: Replace reversed_temp.txt with temp_enc.txt
         mv "temp_enc.txt" "reversed_temp.txt"
     done
-
-    # Clean up temporary files
+    # Clean up: Remove the temporary file temp_rev.txt
     rm "temp_rev.txt"
-
-    # The mystery continues...
-    # The script will continue with more operations that you need to figure out!
 }
 
 # Main Script Execution
@@ -48,8 +30,8 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
-input_file="$1"
-output_file="$2"
+input_file="$1"  # Store the input file path
+output_file="$2" # Store the output file path
 
 # Check if the input file exists
 if [ ! -f "$input_file" ]; then
