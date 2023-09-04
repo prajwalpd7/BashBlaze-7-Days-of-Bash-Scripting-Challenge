00e995516364370146a2795efba06101af6a7e99
nallabellisriparthu <ipsparthu@gmail.com>
2023-09-04 09:37:01 -0700

    added solution

---
+++ Challenges/Day_1/day1-solution.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Task 1 ( completed task 1 comments )
+
+#Task 2
+
+echo "DevOps Zero to Hero"  # completed task 2 echo
+
+#Task 3
+
+variables1="Hello"
+variables2="Dosto"  # completed task 3 Variabeles
+
+#Task 4
+
+variables="$variables1,$variables2"
+echo "$variables welcome to BashBlaze-7-Days-of-Bash-Scripting-Challenge"  # completed task 4 Using variables
+
+#Task 5
+
+echo "1. My current bash path - $BASH"
+echo "2. Bash version I am using - $BASH_VERSION"
+echo "3. PID of bash I am running - $$"
+echo "4. My home directory - $HOME"
+echo "5. Where am I currently? - $PWD"
+echo "6. My hostname - $HOSTNAME"  # Completed task 5 Using Built-in Variables
+
+#Task 6
+
+echo "Files with .txt  to list all the files with a specific extension in a directory"
+ls *.txt
+ls *.sh  # completed task 6 Wildcardss
+
